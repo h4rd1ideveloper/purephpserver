59ccca9eb65af598f421326bb4579da155c5a0c2
Yan Santos Policarpo <policarpo@ice.ufjf.br>
2019-08-08 13:52:31 -0300

    removi o arquivo .env

--- app/.env.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST_PRODUCTION', '177.184.16.61');
-define('DB_TYPE_PRODUCTION', 'mysql');
-define('DB_NAME_PRODUCTION', 'crefazscm_webscm');
-define('DB_PASS_PRODUCTION', 'RBMsuporte03');
-define('DB_USER_PRODUCTION', 'suporteRBM02');
