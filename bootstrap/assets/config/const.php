7d0823e37de69b49e7587ea28aaf469cd3c4cf59
Yan Santos Policarpo <policarpo@ice.ufjf.br>
2019-08-15 17:47:14 -0300

    configurando require_once

--- bootstrap/assets/config/const.php
+++ bootstrap/assets/config/const.php
@@ -1,8 +1,8 @@
 <?php
-define('PRODUCTION_DB_NAME', 'crefazscm_webscm' );
-define('PRODUCTION_DB_USER', 'suporteRBM02' );
-define('PRODUCTION_DB_PASS', 'RBMsuporte03' );
-define('PRODUCTION_DB_TYPE', 'mysql' );
-define('PRODUCTION_DB_HOST', '177.184.16.61' );
-define('ENEL_FIELDS', 'numeroCliente,dvNumeroCliente,codOcorr,valor,numParcelas,referencia,dataVencimento,loteFaturamento,numeroClienteEmpPar,codProduto,empresaParc,codCanalVenda,tipoRetorno,descricaoRetorno,correlativoDocumento,tipoDocumento,dataBaixaPagamento,dataOcorrencia,dataPagamento');
-define('ENEL_TABLE', 'enel_arrecadacao');
+define('PRODUCTION_DB_NAME', getenv('PRODUCTION_DB_NAME') );
+define('PRODUCTION_DB_USER', getenv('PRODUCTION_DB_USER') );
+define('PRODUCTION_DB_PASS', getenv('PRODUCTION_DB_PASS') );
+define('PRODUCTION_DB_TYPE', getenv('PRODUCTION_DB_TYPE') );
+define('PRODUCTION_DB_HOST', getenv('PRODUCTION_DB_HOST') );
+define('ENEL_FIELDS', getenv('ENEL_FIELDS'));
+define('ENEL_TABLE', getenv('ENEL_TABLE'));
--- index.php
+++ index.php
@@ -3,13 +3,7 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ini_set('memory_limit', '1024M');
-define('PRODUCTION_DB_NAME', getenv('PRODUCTION_DB_NAME') );
-define('PRODUCTION_DB_USER', getenv('PRODUCTION_DB_USER') );
-define('PRODUCTION_DB_PASS', getenv('PRODUCTION_DB_PASS') );
-define('PRODUCTION_DB_TYPE', getenv('PRODUCTION_DB_TYPE') );
-define('PRODUCTION_DB_HOST', getenv('PRODUCTION_DB_HOST') );
-define('ENEL_FIELDS', getenv('ENEL_FIELDS'));
-define('ENEL_TABLE', getenv('ENEL_TABLE'));
+require_once 'bootstrap/assets/config/const.php';
 die(
 print_r(
     [
